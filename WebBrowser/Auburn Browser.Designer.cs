a859d32f3aa8f239519d9bfefd008fa95598358c
Darren Phillips <dzp0067@auburn.edu>
2021-03-21 13:54:27 -0500

    Created all controls

---
+++ WebBrowser/Auburn Browser.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WebBrowser
+{
+    partial class WebBrowser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WebBrowserControl = new System.Windows.Forms.WebBrowser();
+            this.TabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.ToolStrip = new System.Windows.Forms.ToolStrip();
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.TabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // WebBrowserControl
+            // 
+            this.WebBrowserControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WebBrowserControl.Location = new System.Drawing.Point(0, 0);
+            this.WebBrowserControl.MinimumSize = new System.Drawing.Size(20, 20);
+            this.WebBrowserControl.Name = "WebBrowserControl";
+            this.WebBrowserControl.Size = new System.Drawing.Size(800, 450);
+            this.WebBrowserControl.TabIndex = 0;
+            // 
+            // TabControl
+            // 
+            this.TabControl.Controls.Add(this.tabPage1);
+            this.TabControl.Controls.Add(this.tabPage2);
+            this.TabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TabControl.Location = new System.Drawing.Point(0, 0);
+            this.TabControl.Name = "TabControl";
+            this.TabControl.SelectedIndex = 0;
+            this.TabControl.Size = new System.Drawing.Size(800, 450);
+            this.TabControl.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 417);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(792, 417);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // ToolStrip
+            // 
+            this.ToolStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.ToolStrip.Location = new System.Drawing.Point(0, 32);
+            this.ToolStrip.Name = "ToolStrip";
+            this.ToolStrip.Size = new System.Drawing.Size(800, 25);
+            this.ToolStrip.TabIndex = 2;
+            this.ToolStrip.Text = "toolStrip1";
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.MenuStrip.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(800, 32);
+            this.MenuStrip.TabIndex = 3;
+            this.MenuStrip.Text = "menuStrip1";
+            // 
+            // WebBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ToolStrip);
+            this.Controls.Add(this.MenuStrip);
+            this.Controls.Add(this.TabControl);
+            this.Controls.Add(this.WebBrowserControl);
+            this.MainMenuStrip = this.MenuStrip;
+            this.Name = "WebBrowser";
+            this.Text = "Auburn Browser";
+            this.TabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser WebBrowserControl;
+        private System.Windows.Forms.TabControl TabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStrip ToolStrip;
+        private System.Windows.Forms.MenuStrip MenuStrip;
+    }
+}
+
--- WebBrowser/Auburn Browser.cs
+++ WebBrowser/Auburn Browser.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WebBrowser
 {
-    public partial class Form1 : Form
+    public partial class WebBrowser : Form
     {
-        public Form1()
+        public WebBrowser()
         {
             InitializeComponent();
         }
